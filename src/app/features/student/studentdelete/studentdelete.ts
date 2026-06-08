2c1671e20e729ad1ba614f5942aae5431d47c2b9
Mustafa Taher <mustafa.taher.abuelnasr@gmail.com>
2026-06-08 15:42:28 +0300

    lab 2

--- src/app/features/student/studentdelete/studentdelete.ts
+++ src/app/features/student/studentdelete/studentdelete.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { Istudent } from '../../../_models/istudent';
+import { StudentService } from '../../../_services/student';
 
 @Component({
   selector: 'app-studentdelete',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './studentdelete.html',
   styleUrl: './studentdelete.css',
 })
-export class Studentdelete {}
+export class Studentdelete implements OnInit {
+  private studentService = inject(StudentService);
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+
+  std: Istudent | undefined;
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.std = this.studentService.getById(id);
+  }
+
+  confirmDelete() {
+    if (this.std) {
+      this.studentService.delete(this.std.id);
+      this.router.navigate(['/students']);
+    }
+  }
+}
--- src/app/features/student/studentedit/studentedit.ts
+++ src/app/features/student/studentedit/studentedit.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { Istudent } from '../../../_models/istudent';
+import { StudentService } from '../../../_services/student';
 
 @Component({
   selector: 'app-studentedit',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './studentedit.html',
   styleUrl: './studentedit.css',
 })
-export class Studentedit {}
+export class Studentedit implements OnInit {
+  private studentService = inject(StudentService);
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+
+  std: Istudent = { id: 0, name: '', age: 0 };
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const found = this.studentService.getById(id);
+    if (found) {
+      this.std = { ...found };
+    }
+  }
+
+  save() {
+    this.studentService.update(this.std);
+    this.router.navigate(['/students']);
+  }
+}
