b3a387aa8fe23986365478228043a0b1f3f8bfb2
Mustafa Taher <mustafa.taher.abuelnasr@gmail.com>
2026-06-15 14:52:35 +0300

    lab 4

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  private router = inject(Router);
+
+  username = '';
+  password = '';
+  error = '';
+
+  login() {
+    if (this.username === 'admin' && this.password === '1234') {
+      localStorage.setItem('isloggedin', 'true');
+      this.router.navigate(['/home']);
+    } else {
+      this.error = 'Invalid username or password.';
+    }
+  }
+}
--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  username = '';
+  password = '';
+  success = false;
+
+  register() {
+    if (this.username && this.password) {
+      this.success = true;
+    }
+  }
+}
