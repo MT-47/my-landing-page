5a94d02b4ba2e58ba32dfb415c8c84f86d03919a
Mustafa Taher <mustafa.taher.abuelnasr@gmail.com>
2026-06-15 15:06:26 +0300

    lab 4

--- src/app/_services/account.ts
+++ src/app/_services/account.ts
@@ -1,14 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class AccountService {
   private key = 'isloggedin';
+
+  isLogged = signal<boolean>(localStorage.getItem(this.key) != null);
 
   login(username: string, password: string): boolean {
     if (username === 'admin' && password === '1234') {
       localStorage.setItem(this.key, 'true');
+      this.isLogged.set(true);
       return true;
     }
     return false;
@@ -16,9 +17,10 @@ export class AccountService {
 
   logout(): void {
     localStorage.removeItem(this.key);
+    this.isLogged.set(false);
   }
 
   isLoggedIn(): boolean {
-    return localStorage.getItem(this.key) != null;
+    return this.isLogged();
   }
 }
--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive, Router } from '@angular/router';
+import { AccountService } from '../_services/account';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +8,12 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  accountService = inject(AccountService);
+  private router = inject(Router);
+
+  logout() {
+    this.accountService.logout();
+    this.router.navigate(['/login']);
+  }
+}
